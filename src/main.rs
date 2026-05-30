30d838c41e8f498084a3f7604ee8809fbf409a3a
Pedro Arthur Hartmann <pedroaphartmann@gmail.com>
2026-05-29 22:52:15 -0300

    parallelization

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,7 @@
 use num_bigint::*;
+use rayon::prelude::*;
+use std::hash::*;
+use std::vec::*;
 
 use crate::file::parameters;
 
@@ -35,15 +38,34 @@ fn step(
     }
 }
 
-fn ord(alpha: &BigUint, modulus: &BigUint) -> BigUint {
-    let mut i = BigUint::ZERO;
-    while i < *modulus {
-        i += 1u8;
-        if alpha.modpow(&i, modulus) == BigUint::from(1u8) {
-            return i
+fn prime_factors(mut n: BigUint) -> Vec<BigUint> {
+    let mut factors = Vec::new();
+    let mut d = BigUint::from(2u8);
+
+    while &d * &d <= n {
+        while &n % &d == BigUint::ZERO {
+            factors.push(d.clone());
+            n /= &d;
+        }
+        d += 1u8;
+    }
+
+    if n > BigUint::from(1u8) {
+        factors.push(n);
+    }
+
+    factors
+}
+fn ord(alpha: &BigUint, p: &BigUint) -> BigUint {
+    let mut n = p - 1u8;
+    for q in prime_factors(n.clone()) {
+        let candidate = &n / &q;
+        if alpha.modpow(&candidate, p) == BigUint::from(1u8) {
+            n = candidate;
         }
     }
-    i
+
+    n
 }
 
 fn sub_mod(a: &BigUint, b: &BigUint, n: &BigUint) -> BigUint {
@@ -54,6 +76,54 @@ fn sub_mod(a: &BigUint, b: &BigUint, n: &BigUint) -> BigUint {
     }
 }
 
+fn try_seed(
+    i: u64,
+    p: &BigUint,
+    alpha: &BigUint,
+    pub_a: &BigUint,
+    pub_b: &BigUint,
+    n: &BigUint,
+) -> Option<BigUint> {
+    let mut hasher = DefaultHasher::new();
+    hasher.write_u64(i);
+    let mut a = hasher.finish().to_biguint()? % n;
+    hasher.write_u64(1);
+    let mut b = hasher.finish().to_biguint()? % n;
+    let mut x = (alpha.modpow(&a, p) * pub_a.modpow(&b, p)) % p;
+
+    let mut x2 = x.clone();
+    let mut a2 = a.clone();
+    let mut b2 = b.clone();
+
+    loop {
+        (x, a, b) = step(x, a, b, p, pub_a, alpha, n);
+
+        (x2, a2, b2) = step(x2, a2, b2, p, pub_a, alpha, n);
+        (x2, a2, b2) = step(x2, a2, b2, p, pub_a, alpha, n);
+
+        if x == x2 {
+            break;
+        }
+    }
+
+    let r = sub_mod(&b, &b2, n);
+
+    if r == BigUint::ZERO {
+        return None;
+    }
+
+    let r_inv = r.modinv(n)?;
+
+    let priv_key = (r_inv * sub_mod(&a2, &a, n)) % n;
+
+    if alpha.modpow(&priv_key, p) != *pub_a {
+        None
+    } else {
+        Some(pub_b.modpow(&priv_key, p))
+    }
+}
+
+
 fn main() {
     let number_problem_str = std::env::args_os().nth(1).unwrap_or_default();
     if number_problem_str.is_empty() {
@@ -62,42 +132,18 @@ fn main() {
 
     let number_problem = number_problem_str.into_string().unwrap().parse::<u32>().unwrap();
     let (p, alpha, pub_a, pub_b) = parameters(number_problem, "desafios.txt");
+    println!("p\t= {p}");
+    println!("alpha\t= {alpha}");
+    println!("pub_a\t= {pub_a}");
+    println!("pub_b\t= {pub_b}");
     let n = ord(&alpha, &p);
+    println!("ord\t= {n}");
     
-    let mut x: BigUint;
-    let mut a = BigUint::from(0u8);
-    let mut b: BigUint;
-    let mut x2: BigUint;
-    let mut a2 = BigUint::from(0u8);
-    let mut b2: BigUint;
-    let mut r = BigUint::from(0u8);
+    let result = (1u64..u64::MAX)
+        .into_par_iter()
+        .find_map_first(|i| {
+            try_seed(i, &p, &alpha, &pub_a, &pub_b, &n)
+        });
 
-    for i in 1.. {
-        a = i.to_biguint().unwrap();
-        b = i.to_biguint().unwrap();
-        x = (alpha.modpow(&a, &p) * pub_a.modpow(&b, &p)) % &p;
-
-        x2 = x.clone();
-        a2 = a.clone();
-        b2 = b.clone();
-
-        loop {
-            (x, a, b) = step(x, a, b, &p, &pub_a, &alpha, &n);
-
-            (x2, a2, b2) = step(x2, a2, b2, &p, &pub_a, &alpha, &n);
-            (x2, a2, b2) = step(x2, a2, b2, &p, &pub_a, &alpha, &n);
-
-            if x == x2 {
-                break;
-            }
-        }
-
-        r = sub_mod(&b, &b2, &n);
-        if r != BigUint::ZERO {
-            break
-        }
-    }
-    let r_inv = r.modinv(&n).unwrap();
-    let alice_private_key = (r_inv * sub_mod(&a2, &a, &n)) % &n;
-    println!("k_ab = {}", pub_b.modpow(&alice_private_key, &p))
+    println!("k_ab\t= {}", result.unwrap());
 }
